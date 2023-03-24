d26e8c682eaf3d2fe08a448504675978fed2e31b
PedroHGaspar <ordep12@hotmail.com.br>
2023-03-24 13:56:44 -0300

    Fetch and Fetcher. API is free

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import RAPID_API_KEY from '../inv.env'
 
 const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
@@ -11,7 +10,7 @@ const useFetch = (endpoint, query) => {
         method: "GET",
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            "X-RapidAPI-Key": RAPID_API_KEY,
+            "X-RapidAPI-Key": "2461af710amsha07de7d052b72c8p1a08dcjsnffe4645de012",
             "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
         },
         params: { ...query },
--- hook/useFetcher.js
+++ hook/useFetcher.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import RAPID_API_KEY_TWO from '../inv.env'
 
 const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
@@ -11,7 +10,7 @@ const useFetch = (endpoint, query) => {
         method: "GET",
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            "X-RapidAPI-Key": RAPID_API_KEY_TWO,
+            "X-RapidAPI-Key": "992bf20958msh4d98cc6b34914edp1fd6efjsnda1db23fe804",
             "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
         },
         params: { ...query },
